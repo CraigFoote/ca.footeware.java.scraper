186fe4a0d71fef92ee5d95171a886f20c49d7fdd
Craig Foote <CraigFoote@gmail.com>
2026-05-02 17:00:02 -0400

    Use streaming parser to conserve memory

--- src/main/java/ca/footeware/java/scraper/EgoScraper.java
+++ src/main/java/ca/footeware/java/scraper/EgoScraper.java
@@ -4,9 +4,8 @@ import java.io.IOException;
 import java.util.Scanner;
 
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.jsoup.parser.StreamParser;
 
 /**
  * Scrapes extensions.gnome.org's review page for provided name and reports its
@@ -14,41 +13,50 @@ import org.jsoup.select.Elements;
  */
 public class EgoScraper {
 
+	private static final String SERVER_STATUS = "Server Status Indicator";
 	private static final String URL = "https://extensions.gnome.org/review/";
-	private static final String SUCCESS = "%s is in position %d of %d at %s.";
+	private static final String SUCCESS = "'%s' is in position %d at %s.";
+	private static final String FAIL = "Extension named '%s' not found at %s.";
 
 	/**
-	 * @throws IOException if JSoup cannot connect to EGO's reviews page
+	 * @throws IOException if JSoup cannot connect to EGO's review page
 	 */
 	public static void main() throws IOException {
 		Scanner scanner = new Scanner(System.in);
 		String soughtName = null;
-		doLoop: do {
-			System.out.println("Enter extension name ('q' to quit):");
+		do {
+			System.out.println("Enter extension name (press Enter for 'Server Status Indicator' or 'q' to quit):");
 			soughtName = scanner.nextLine();
-			if (soughtName.equals("q")) {
-				break doLoop;
+			if (soughtName.isEmpty()) {
+				soughtName = SERVER_STATUS;
 			}
-			Document doc = Jsoup.connect(URL).get();
-			Element ul = doc.select("ul.extensions").getFirst();
-			Elements extensions = ul.select("li");
+			if (soughtName.equals("q")) {
+				break;
+			}
+
 			int counter = 0;
 			boolean found = false;
-			forLoop: for (Element extension : extensions) {
-				counter++;
-				String extensionName = extension.selectFirst("h3").selectFirst("a").text();
-				if (soughtName.equals(extensionName)) {
-					String message = String.format(SUCCESS, extensionName, counter, extensions.size(), URL);
-					System.out.println(message);
-					found = true;
-					break forLoop;
+			try (StreamParser streamer = Jsoup.connect(URL).execute().streamParser()) {
+				Element extension;
+				while ((extension = streamer.selectNext("li.extension")) != null) {
+					counter++;
+					String extensionName = extension.selectFirst("h3").selectFirst("a").text();
+					if (soughtName.equals(extensionName)) {
+						String message = String.format(SUCCESS, extensionName, counter, URL);
+						System.out.println(message);
+						found = true;
+						break;
+					}
+					extension.remove(); // Keep memory usage low by discarding processed elements
 				}
 			}
+
 			if (!found) {
-				System.err.println(soughtName + " not found at " + URL + ".");
+				String message = String.format(FAIL, soughtName, URL);
+				System.err.println(message);
 			}
 		} while (!"q".equals(soughtName));
 		scanner.close();
-		System.out.println("Toodles!");
+		System.out.println("Toodles! Send money!");
 	}
 }
